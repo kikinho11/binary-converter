46aefbfabbbce32cf1d5fda2296c407215b8bd09
kikinho11 <fca.10.fa@gmail.com>
2021-03-05 17:49:59 +0000

    SideBar Code updated

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import '../style/SideBar.css';
 
 export default function SideBar()
 {
     return(
-        <Container>
-            <Row>
+        <Container fluid className="sidebarContainer">
+            <Row className="sidebarTitle">
                 <h4>Binary Converter</h4>
             </Row>
 
-            <Row>
+            <Row className="sidebarItem">
                 <a href="/" >Home </a>
             </Row>
 
-            <Row>
+            <Row className="sidebarItem">
                 <a href="/converter" >Converter </a>
             </Row>
         </Container>
