48eaf17cf0bdba21f776bcf3cf454eadf9321ba5
kikinho11 <fca.10.fa@gmail.com>
2021-03-05 17:50:22 +0000

    Updated with binary converter

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,6 +1,59 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+
 export default function Converter()
 {
+
+    const [userInput, setUserInput] = useState("0");
+    const [binaryEquivalent, setBinaryEquivalent] = useState("0");
+
+    function convertToBinary()
+    {
+
+        if (isNaN(userInput))
+        {
+            alert("Insert a valid number");
+            setBinaryEquivalent("0");
+            setUserInput("0");
+            return;
+        }
+
+        var binaryNumber = Number(userInput).toString(2)
+        setBinaryEquivalent(binaryNumber);
+    }
+
     return (
-        <h4>This is a converter!</h4>
+        <Container fluid>
+            <Row className="converterRow text-center">
+                <Col>
+                    <form>
+                        <h5>Enter decimal number</h5>
+                        <input
+                            name="decimal number imput"
+                            placeholder="Enter a Decimal Number"
+                            autoComplete="off"
+                            maxLength="16"
+                            value={userInput}
+                            onChange={ (element) => setUserInput(element.target.value) }
+                        />
+                        <button
+                            className="btn btn-success"
+                            type="button"
+                            onClick={ () => convertToBinary() }
+                        >
+                            
+                            To Binary
+                        </button>
+                    </form>
+                </Col>
+            </Row>
+
+            <Row className="text-center">
+                <Col>
+                    <h5>Binary Equivalent </h5>
+                    <h5>{binaryEquivalent}</h5>
+                </Col>
+            </Row>
+        </Container>
     );
 }
